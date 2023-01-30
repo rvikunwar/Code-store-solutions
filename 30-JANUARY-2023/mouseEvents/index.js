0904954a5162cf50a9d60bc814f968555b9d1660
Ravindra Kunwar <ravindra.k@codestoresolutions.com>
2023-01-30 19:30:52 +0530

    30 jan

--- 30-JANUARY-2023/mouseEvents/index.js
+++ 30-JANUARY-2023/mouseEvents/index.js
@@ -1,44 +1,65 @@
+let text = document.getElementById("text")
+
 function onClickHandler(){
-    alert("	A user clicks on an button")
+    text.innerHTML = "	A user clicked on an button"
+    // alert("	A user clicks on an button")
 }
 
 function onContextmenuHandler(){
-    alert("A user right-clicks on an button")
+    text.innerHTML = "A user right-clicks on an button"
+
+    // alert("A user right-clicks on an button")
 }
 
 function onDblclickHandler(){
-    alert("A user double-clicks on an button")
+    text.innerHTML = "A user double-clicks on an button"
+
+    // alert("A user double-clicks on an button")
 }
 
 function onmousedownHandler(){
-    alert("A mouse button is pressed over an button")
+    text.innerHTML = "A mouse button is pressed over an button"
+
+    // alert("A mouse button is pressed over an button")
 }
 
 function onmouseenterHandler(){
-    alert("The mouse pointer moves into an button")
+    text.innerHTML = "The mouse pointer moves into an button"
+
+    // alert("The mouse pointer moves into an button")
 }
 
 
 function onmouseleaveHandler(){
-    alert("The mouse pointer moves out of an button")
+    text.innerHTML = "The mouse pointer moves out of an button"
+
+    // alert("The mouse pointer moves out of an button")
 }
 
 
 function onmousemoveHandler(){
-    alert("The mouse pointer moves over an button")
+    text.innerHTML = "The mouse pointer moves over an button"
+
+    // alert("The mouse pointer moves over an button")
 }
 
 
 function onmouseoutHandler(){
-    alert("The mouse pointer moves out of an button")
+    text.innerHTML = "The mouse pointer moves out of an button"
+
+    // alert("The mouse pointer moves out of an button")
 }
 
 
 function onmouseoverHandler(){
-    alert("The mouse pointer moves onto an button")
+    text.innerHTML = "The mouse pointer moves onto an button"
+
+    // alert("The mouse pointer moves onto an button")
 }
 
 
 function onmouseupHandler(){
-    alert("A mouse button is released over an button")
+    text.innerHTML = "A mouse button is released over an button"
+
+    // alert("A mouse button is released over an button")
 }

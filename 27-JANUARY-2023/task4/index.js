eb44d7b7614592f82747971c19bc6b931743e9dc
Ravindra Kunwar <ravindra.k@codestoresolutions.com>
2023-01-30 11:40:00 +0530

    30 Jan

--- 27-JANUARY-2023/task4/index.js
+++ 27-JANUARY-2023/task4/index.js
@@ -1,8 +1,11 @@
-var cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"];
+var cities = ["New York", "Los Angeles", "Chicago", "Kolkata", "Chennai", "Mumbai", "Jaipur", "San Diego", "Dallas", "San Jose"];
+var routesArray = []
 
 function disableCity() {
     var from = document.getElementById("from").value;
     var to = document.getElementById("to").value;
+    let filteredToCity;
+    let filteredFromCity;
 
     if (from == to) {
         alert("You cannot travel from your own city!");
@@ -10,17 +13,37 @@ function disableCity() {
         document.getElementById("to").value = "None";
     }
 
+    if(from){
+        filteredToCity = routesArray.filter((city) => city.from == from)
+    } if(from){
+        filteredFromCity = routesArray.filter((city) => city.to == to)
+    } 
+
+    // console.log(filteredFromCity, filteredToCity)
+
     for (var i = 0; i < cities.length; i++) {
-        console.log(from, '==', cities[i])
+        // console.log(from, '==', cities[i])
+        if(filteredFromCity){
+            filteredFromCity.forEach((city) => {
+                if(city.from == cities[i]){
+                    document.getElementById("from").options[i+1].disabled = true;
+                }
+            })
+        }
+        if(filteredToCity){
+            filteredToCity.forEach((city) => {
+                if(city.to == cities[i]){
+                    console.log(city.to, cities[i])
+                    document.getElementById("to").options[i+1].disabled = true;
+                }
+            })
+        }
+
         if (cities[i] == from) {
             // console.log(document.getElementById("to").options[i+1])
             document.getElementById("to").options[i+1].disabled = true;
         } else if (cities[i] == to) {
             document.getElementById("from").options[i+1].disabled = true;
-        } else {
-            document.getElementById("to").options[i+1].disabled = false;
-            document.getElementById("from").options[i+1].disabled = false;
-
         }
     }
 }
@@ -42,7 +65,19 @@ function addData() {
         return;
     }
 
+    routesArray.push({
+        from, 
+        to
+    })
+
     var newRoute = document.createElement("li");
     newRoute.innerHTML = from + " - " + to;
     route.appendChild(newRoute);
+    document.getElementById("from").value = "None";
+    document.getElementById("to").value = "None";
+
+    for (var i = 0; i < cities.length; i++) {
+        document.getElementById("to").options[i+1].disabled = false;
+        document.getElementById("from").options[i+1].disabled = false;
+    }
 }
